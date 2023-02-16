de91d336b0931991bff8849f927bec2ca68369e2
Adooz <kndonake@gmail.com>
2023-02-16 15:38:29 -0800

    Second C program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print out sizes of data types in c
+ * code by Adooz Tech. Kingskey Ndonake on slack
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizef(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizef(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizef(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizef(d);
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizef(e));
+	return (0);
+}
